919a4cae2310a410c3f4f8a9bae28a9abafe0811
mishgan55 <vorota.mogilev@gmail.com>
2022-04-28 01:52:45 +0300

    First Commit

---
+++ src/concurrent/homework/Operator.java
@@ -0,0 +1,82 @@
+package concurrent.homework;
+
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Operator {
+    private final Semaphore semaphore;
+
+    private final Lock operatorLock;
+
+    private boolean freeOperator[];
+
+    private ConcurrentLinkedQueue<Operator> operator;
+
+    public Operator() {
+
+        semaphore = new Semaphore(3);
+        freeOperator = new boolean[4];
+        operatorLock = new ReentrantLock();
+        Arrays.fill(freeOperator, true);
+
+    }
+
+    public void conversation(Object client) throws InterruptedException {
+        try {
+            semaphore.acquire();
+            int assignedOperator = getOperator();
+
+            Thread.sleep(1000);
+
+            System.out.println("Operator " + assignedOperator +
+                    " start conversation with  " + Thread.currentThread().getName());
+
+            Thread.sleep(1000);
+
+            releaseOperator(assignedOperator);
+
+            System.out.println("Operator" + assignedOperator +
+                    " finished the conversation with the " + Thread.currentThread().getName());
+
+            semaphore.release();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private int getOperator() throws InterruptedException {
+        int foundOperator = -1;
+        operatorLock.lock();
+        for (int i = 1; i < freeOperator.length; i++) {
+            Thread.sleep(1000);
+            if (freeOperator[i]) {
+                foundOperator = i;
+                freeOperator[i] = false;
+                System.out.println("Operator " + i + " is connects with a new client");
+                break;
+
+
+            }
+        }
+        operatorLock.unlock();
+        return foundOperator;
+
+
+    }
+
+    private void releaseOperator(int operator) throws InterruptedException {
+        operatorLock.lock();
+        Thread.sleep(1000);
+        freeOperator[operator] = true;
+        operatorLock.unlock();
+
+    }
+
+
+}
